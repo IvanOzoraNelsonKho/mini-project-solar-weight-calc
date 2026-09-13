f610cea4901db8ca88ab59f3a3bf9ec697ad6ea3
KennyMartinHoliem <Kennyholiem07@gmail.com>
2026-09-13 14:22:45 +0700

    merapikan kode

--- logic.js
+++ logic.js
@@ -1,32 +1,29 @@
-class HitungBerat {
-    constructor() {
-        this.gBumi = 9.8;
-        this.dataGravitasi = {
-            "merkurius": 3.7,
-            "venus": 8.87,
-            "mars": 3.71,
-            "jupiter": 24.79,
-            "saturnus": 10.44,
-            "uranus": 8.69,
-            "neptunus": 11.15
-        };
-    }
-
-    hitung(massa, idPlanet) {
-        let gPlanet = this.dataGravitasi[idPlanet];
-        
-        if (!gPlanet) {
-            return {
-                error: "Silakan pilih planet terlebih dahulu!"
-            };
-        }
-
-        let hasilBerat = massa * (gPlanet / this.gBumi);
+const gBumi = 9.8;
+const dataGravitasi = {
+    "merkurius": 3.7,
+    "venus": 8.87,
+    "mars": 3.71,
+    "jupiter": 24.79,
+    "saturnus": 10.44,
+    "uranus": 8.69,
+    "neptunus": 11.15
+};
+  
+function hitungBerat(massa, idPlanet) {
+    let gPlanet = dataGravitasi[idPlanet];
+    
+    if (!gPlanet) {
         return {
-            berat: hasilBerat
+            error: "Silakan pilih planet terlebih dahulu!"
         };
     }
+
+    let hasilBerat = massa * (gPlanet / gBumi);
+    return {
+        berat: hasilBerat
+    };
 }
+
 
 
 
